e7ee7739db2cf1152fd353542178efcd0c6e8418
cs <cs.temporary@icloud.com>
2020-08-28 23:40:51 +0000

    Require vivarium-core >= 0.0.12
    vivarium-cell's death process relies on an updated version of update_in
    from core.experiment that was introduced in v0.0.12.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     package_data={},
     include_package_data=True,
     install_requires=[
-        'vivarium-core',
+        'vivarium-core>=0.0.12',
         'cobra',
         'Arpeggio',
         'parsimonious',
