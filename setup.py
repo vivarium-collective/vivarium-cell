c0986a9cbb7a9738e2c35f3ae30496b5e24c1d84
Ryan Spangler <ryan.spangler@gmail.com>
2020-08-31 13:24:52 -0700

    version 0.0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", 'r') as readme:
 
 setup(
     name='vivarium-cell',
-    version='0.0.1',
+    version='0.0.2',
     packages=[
         'cell',
         'cell.bigg_models',
