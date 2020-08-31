7425cf3a890d9b593cbfb05e014eb255dbbbe27f
Ryan Spangler <ryan.spangler@gmail.com>
2020-08-31 13:36:49 -0700

    update release to clear dist

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", 'r') as readme:
 
 setup(
     name='vivarium-cell',
-    version='0.0.2',
+    version='0.0.3',
     packages=[
         'cell',
         'cell.bigg_models',
