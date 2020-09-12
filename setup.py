bb564896484c69afe7be198de15131b7048c2dfc
Eran Agmon <agmon.eran@gmail.com>
2020-09-11 23:29:36 -0700

    bump version;

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", 'r') as readme:
 
 setup(
     name='vivarium-cell',
-    version='0.0.14',
+    version='0.0.15',
     packages=[
         'cell',
         'cell.bigg_models',
