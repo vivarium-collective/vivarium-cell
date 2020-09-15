7a5526612c2779ace4ac2e7bcdf94bc724c8f267
Ryan Spangler <ryan.spangler@gmail.com>
2020-09-15 10:43:30 -0700

    version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", 'r') as readme:
 
 setup(
     name='vivarium-cell',
-    version='0.0.16',
+    version='0.0.17',
     packages=[
         'cell',
         'cell.bigg_models',
@@ -38,7 +38,7 @@ setup(
     long_description_content_type='text/markdown',
     include_package_data=True,
     install_requires=[
-        'vivarium-core>=0.0.13',
+        'vivarium-core>=0.0.19',
         'cobra',
         'Arpeggio',
         'parsimonious',
