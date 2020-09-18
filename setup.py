ee20b3a4fbcf62f5768cbfcf5b2209c4e32a10ee
cs <cs.temporary@icloud.com>
2020-09-18 14:23:25 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", 'r') as readme:
 
 setup(
     name='vivarium-cell',
-    version='0.0.17',
+    version='0.0.18',
     packages=[
         'cell',
         'cell.bigg_models',
