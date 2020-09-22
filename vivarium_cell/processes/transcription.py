c104785a96f2a33c85b8f498dfb3911abae3d46a
Eran Agmon <agmon.eran@gmail.com>
2020-09-21 22:32:45 -0700

    transcription and translation codstring tests

--- vivarium_cell/processes/transcription.py
+++ vivarium_cell/processes/transcription.py
@@ -66,7 +66,7 @@ class Transcription(Process):
 
         * **chromosome**: The linked :term:`store` should hold a
           representation of the chromosome in the form returned by
-          :py:meth:`cell.states.chromosome.Chromosome.to_dict`.
+          :py:meth:`vivarium_cell.states.chromosome.Chromosome.to_dict`.
         * **molecules**: Expects variables with the names in the
           *molecule_ids* configuration. These are the monomers consumed
           by transcription.
@@ -187,7 +187,7 @@ class Transcription(Process):
         ... }
         >>> update = transcription_process.next_update(1.0, state)
         >>> print(update['chromosome'])
-        {'rnaps': {'_add': [{'path': (2,), 'state': <class 'cell.states.chromosome.Rnap'>: {'id': 2, 'template': 'pA', 'template_index': 0, 'terminator': 1, 'domain': 0, 'state': 'polymerizing', 'position': 7}}, {'path': (3,), 'state': <class 'cell.states.chromosome.Rnap'>: {'id': 3, 'template': 'pB', 'template_index': 1, 'terminator': 0, 'domain': 0, 'state': 'occluding', 'position': 3}}, {'path': (4,), 'state': <class 'cell.states.chromosome.Rnap'>: {'id': 4, 'template': 'pA', 'template_index': 0, 'terminator': 0, 'domain': 0, 'state': 'occluding', 'position': 0}}], '_delete': []}, 'rnap_id': 4, 'domains': {0: <class 'cell.states.chromosome.Domain'>: {'id': 0, 'lead': 0, 'lag': 0, 'children': []}}, 'root_domain': 0}
+        {'rnaps': {'_add': [{'path': (2,), 'state': <class 'vivarium_cell.states.chromosome.Rnap'>: {'id': 2, 'template': 'pA', 'template_index': 0, 'terminator': 1, 'domain': 0, 'state': 'polymerizing', 'position': 7}}, {'path': (3,), 'state': <class 'vivarium_cell.states.chromosome.Rnap'>: {'id': 3, 'template': 'pB', 'template_index': 1, 'terminator': 0, 'domain': 0, 'state': 'occluding', 'position': 3}}, {'path': (4,), 'state': <class 'vivarium_cell.states.chromosome.Rnap'>: {'id': 4, 'template': 'pA', 'template_index': 0, 'terminator': 0, 'domain': 0, 'state': 'occluding', 'position': 0}}], '_delete': []}, 'rnap_id': 4, 'domains': {0: <class 'vivarium_cell.states.chromosome.Domain'>: {'id': 0, 'lead': 0, 'lag': 0, 'children': []}}, 'root_domain': 0}
         '''
 
         if not initial_parameters:
