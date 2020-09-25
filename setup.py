b4c8b7903ac6b2c5abfaa694f13eb7d28029c5a8
Eran Agmon <agmon.eran@gmail.com>
2020-09-25 07:45:15 -0700

    register global deriver

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", 'r') as readme:
 
 setup(
     name='vivarium-cell',
-    version='0.0.21',
+    version='0.0.22',
     packages=[
         'vivarium_cell',
         'vivarium_cell.analysis',
--- vivarium_cell/__init__.py
+++ vivarium_cell/__init__.py
@@ -1,13 +1,23 @@
 import numpy as np
 
 from vivarium.library.units import Quantity, units
-from vivarium.core.registry import updater_registry
+from vivarium.core.registry import (
+    updater_registry,
+    process_registry,
+)
 from vivarium_cell.library.lattice_utils import (
     get_bin_site,
     get_bin_volume,
     count_to_concentration,
 )
 
+# import processes
+from vivarium_cell.processes.derive_globals import DeriveGlobals
+
+# register processes
+process_registry.register(DeriveGlobals.name, DeriveGlobals)
+
+# register updaters
 def update_field_with_exchange(current_value, new_value, states):
     '''Update environment with agent exchange
 
