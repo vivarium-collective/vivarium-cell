fa252847756bc6fe94b49844369769273917381a
Eran Agmon <agmon.eran@gmail.com>
2020-11-11 08:57:14 -0800

    v0.0.25
    adding michael.py for testing

---
+++ vivarium_cell/processes/michael.py
@@ -0,0 +1,146 @@
+'''
+Execute by running: ``python template/process/template_process.py``
+
+TODO: Replace the template code to implement your own process.
+'''
+
+import os
+
+from vivarium.core.process import Process
+from vivarium.core.composition import (
+    simulate_process_in_experiment,
+    PROCESS_OUT_DIR,
+)
+from vivarium.plots.simulation_output import plot_simulation_output
+
+
+# a global NAME is used for the output directory and for the process name
+NAME = 'template'
+
+
+class Template(Process):
+    '''
+    This mock process provides a basic template that can be used for a new process
+    '''
+
+    # give the process a name, so that it can register in the process_repository
+    name = NAME
+
+    # declare default parameters as class variables
+    defaults = {
+        'uptake_rate': 0.1,
+    }
+
+    def __init__(self, parameters=None):
+        # parameters passed into the constructor merge with the defaults
+        # and can be access through the self.parameters class variable
+        super(Template, self).__init__(parameters)
+
+    def ports_schema(self):
+        '''
+        ports_schema returns a dictionary that declares how each state will behave.
+        Each key can be assigned settings for the schema_keys declared in Store:
+
+        * `_default`
+        * `_updater`
+        * `_divider`
+        * `_value`
+        * `_properties`
+        * `_emit`
+        * `_serializer`
+        '''
+
+        return {
+            'internal': {
+                'A': {
+                    '_default': 1.0,
+                    '_updater': 'accumulate',
+                    '_emit': True,
+                },
+            },
+            'external': {
+                'A': {
+                    '_default': 1.0,
+                    '_updater': 'accumulate',
+                    '_emit': True,
+                },
+            },
+        }
+        
+
+    def next_update(self, timestep, states):
+
+        # get the states
+        internal_A = states['internal']['A']
+        external_A = states['external']['A']
+
+        # calculate timestep-dependent updates
+        internal_update = self.parameters['uptake_rate'] * external_A * timestep
+        external_update = -1 * internal_update
+
+        # return an update that mirrors the ports structure
+        return {
+            'internal': {
+                'A': internal_update},
+            'external': {
+                'A': external_update}
+        }
+
+
+# functions to configure and run the process
+def run_template_process():
+    '''Run a simulation of the process.
+
+    Returns:
+        The simulation output.
+    '''
+
+    # initialize the process by passing in parameters
+    parameters = {}
+    template_process = Template(parameters)
+
+    # declare the initial state, mirroring the ports structure
+    initial_state = {
+        'internal': {
+            'A': 0.0
+        },
+        'external': {
+            'A': 1.0
+        },
+    }
+
+    # run the simulation
+    sim_settings = {
+        'total_time': 10,
+        'initial_state': initial_state}
+    output = simulate_process_in_experiment(template_process, sim_settings)
+
+    return output
+
+
+def test_template_process():
+    '''Test that the process runs correctly.
+
+    This will be executed by pytest.
+    '''
+    output = run_template_process()
+    # TODO: Add assert statements to ensure correct performance.
+
+
+def main():
+    '''Simulate the process and plot results.'''
+    # make an output directory to save plots
+    out_dir = os.path.join(PROCESS_OUT_DIR, NAME)
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    output = run_template_process()
+
+    # plot the simulation output
+    plot_settings = {}
+    plot_simulation_output(output, plot_settings, out_dir)
+
+
+# run module with python template/process/template_process.py
+if __name__ == '__main__':
+    main()
