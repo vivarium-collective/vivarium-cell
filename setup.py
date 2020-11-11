1806810cd32dcd34e04979a8beb3ff97c85bb745
Eran Agmon <agmon.eran@gmail.com>
2020-11-11 08:57:14 -0800

    v0.0.25

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", 'r') as readme:
 
 setup(
     name='vivarium-cell',
-    version='0.0.24',
+    version='0.0.25',
     packages=[
         'vivarium_cell',
         'vivarium_cell.analysis',
