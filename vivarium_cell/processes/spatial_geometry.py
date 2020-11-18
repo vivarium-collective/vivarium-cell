e9d435071ee462a941b12fd56908a0f5fa59e943
juliaschaepe <jmschaepe@gmail.com>
2020-11-18 09:29:21 -0800

    Progress on spatial_grometry.py however this is still unfinished

--- vivarium_cell/processes/spatial_geometry.py
+++ vivarium_cell/processes/spatial_geometry.py
@@ -1,10 +1,10 @@
 '''
 Execute by running: ``python vivarium/process/template_process.py``
-
-TODO: Replace the template code to implement your own process.
+Note: This process is unfinished.
 '''
 
 import os
+import numpy as np
 
 from vivarium.core.process import Deriver
 from vivarium.core.composition import (
@@ -13,24 +13,40 @@ from vivarium.core.composition import (
 )
 from vivarium.plots.simulation_output import plot_simulation_output
 
+'''
+References:
+ - width of 0.73 um: Cluzel et al., Nucleic Acids Research (2008)
+	- strain: K-12 Frag1
+ - periplasm width of 21 nm: Matias et al., J Bacteriology (2003)
+ - cell density of 1100 g/L (from WCM): Baldwin et al., Arch microbiol. (1995)
+'''
 
 NAME = 'spatial_geometry'
-
+WIDTH = 0.73		# in um
+PERIPLASM_WIDTH = 21        # in nm
+DENSITY = 1100      # fg/um^3
 
 class SpatialGeometry(Deriver):
     """
-    Assumes cylinder
+    Assumes spherocylinder with constant radius
     """
 
     name = NAME
     defaults = {
         'nodes': [],
         'edges': {},
-        'density': 0.0,
+        'density': DENSITY,
+        'mw': {},
+        'width': WIDTH,
     }
 
     def __init__(self, parameters=None):
         super(SpatialGeometry, self).__init__(parameters)
+        self.nodes = np.asarray(self.parameters['nodes'])
+        self.edges = self.parameters['edges']
+        self.mw = self.parameters['mw']
+        self.molecule_ids = self.parameters['mw'].keys()
+        self.density = self.parameters['density']
 
     def initial_state(self, config=None):
         pass
@@ -56,6 +72,7 @@ class SpatialGeometry(Deriver):
         }
         edge_schema = {
             edge_id: {
+                'nodes': [],
                 'cross_sectional_area': 1.0,
             } for edge_id in self.parameters['edges'].keys()
         }
@@ -63,12 +80,71 @@ class SpatialGeometry(Deriver):
         return {**node_schema, **edge_schema}
 
     def next_update(self, timestep, states):
+        volume = np.zeros(len(self.nodes))
+        length = [states['nodes'][node_id]['length'] for node_id in self.nodes]
+        cross_sectional_area = np.zeros(len(self.edges))
+        inner_radius = states['nodes']['nucleoid']['radius']
+        outer_radius = states['nodes']['periplasm']['radius'] + inner_radius
 
         # TODO -- get volume of each node from molecules and density
+        for i, node_id in enumerate(self.nodes):
+            nodes = states[node_id]['nodes']
+            # if nucleoid or nucleoid region of membrane
+            nucleoid = True
+            volume[i] = sum(array_from(states[node_id]['molecules']))*self.density
+            if nucleoid:
+                length[i] = volume[i]/(np.pi * inner_radius**2)
+        for i, edge_id in enumerate(self.edges):
+            nodes = states[edge_id]['nodes']
+            nucleoid = 'nucleoid' in nodes
+            cytosol = 'cytosol_front' or 'cytosol_rear' in nodes
+            periplasm = 'periplasm' in nodes
+            cytosol_inner_membrane = False
+            cytosol_outer_membrane = False
+            nucleoid_inner_membrane = False
+            nucleoid_outer_membrane = False
 
-        #
+            # series of if statements for each interface
+            if nucleoid and cytosol:
+                cross_sectional_area[i] = np.pi * inner_radius**2
+                continue
+                # find if node is membrane
+            if nucleoid and nucleoid_inner_membrane:
+                node_id = 'membrane'
+                cross_sectional_area[i] = 2 * np.pi * inner_radius * states[
+                    'nodes'][node_id]['length']
+                continue
+            if cytosol and cytosol_inner_membrane:
+                cross_sectional_area[i] = 2 * np.pi * inner_radius**2
+                continue
+            if periplasm and nucleoid_inner_membrane:
+                cross_sectional_area[i] = 2 * np.pi * inner_radius * states[
+                        'nodes'][node_id]['length']
+                continue
+            if periplasm and nucleoid_outer_membrane:
+                cross_sectional_area[i] = 2 * np.pi * outer_radius * states[
+                    'nodes'][node_id]['length']
+                continue
+            if periplasm and cytosol_inner_membrane:
+                cross_sectional_area[i] = 2 * np.pi * inner_radius**2
+                continue
+            if periplasm and cytosol_outer_membrane:
+                cross_sectional_area[i] = 2 * np.pi * outer_radius**2
+                continue
 
-        return {}
+        node_update = {
+            node_id: {
+                'volume': volume[np.where(self.nodes == node_id)][0],
+                'length': volume[np.where(self.nodes == node_id)][0],
+            } for node_id in self.nodes
+        }
+        edge_update = {
+            edge_id: {
+                'cross_sectional_area': cross_sectional_area[
+                    np.where(self.edges == edge_id)][0],
+            } for edge_id in self.nodes
+        }
+        return {**node_update, **edge_update}
 
 
 
@@ -122,6 +198,16 @@ def main():
     plot_settings = {}
     plot_simulation_output(output, plot_settings, out_dir)
 
+
+# Helper functions
+def array_from(d):
+    return np.array(list(d.values()))
+
+
+def array_to(keys, array):
+    return {
+        key: array[index]
+        for index, key in enumerate(keys)}
 
 # run module with python vivarium/process/template_process.py
 if __name__ == '__main__':
