6850baff4617f98397a32bef39b70105676b0cda
Michael Yang <michaelyang42@gmail.com>
2020-12-05 17:48:23 -0500

    Model incorporates behavior in all growth phases

--- vivarium_cell/processes/michael.py
+++ vivarium_cell/processes/michael.py
@@ -12,10 +12,17 @@ from vivarium.core.composition import (
 )
 from vivarium.plots.simulation_output import plot_simulation_output
 from enum import IntEnum
+from numpy.random import exponential
 
 # a global NAME is used for the output directory and for the process name
 NAME = 'HairGrowth'
 
+
+class Phase(IntEnum):
+    ANAGEN   = 1
+    CATAGEN  = 2
+    TELOGEN1 = 3
+    TELOGEN2 = 4
 
 class HairGrowth(Process):
     '''
@@ -34,6 +41,8 @@ class HairGrowth(Process):
         'q_T1_T2': 1 / (4 * 30.437),  # rate constant (T1 to T2)
         'q_T2_A': 1 / (2 * 7),  # rate constant (T2 to A)
     }
+
+    next_phase_change = None
 
     def __init__(self, parameters=None):
         # parameters passed into the constructor merge with the defaults
@@ -57,12 +66,12 @@ class HairGrowth(Process):
         return {
             'global': {
                 'length': {
-                    '_default': 0,
+                    '_default': 0.0,
                     '_updater': 'accumulate',
                     '_emit': True,
                 },
                 'phase': {
-                    '_default': 'anagen',
+                    '_default': Phase.ANAGEN,
                     '_updater': 'set',
                     '_emit': True,
                 },
@@ -73,16 +82,48 @@ class HairGrowth(Process):
         # get the states
         length = states['global']['length']
         phase = states['global']['phase']
+        q = [self.parameters['q_A_C'],
+             self.parameters['q_C_T1'],
+             self.parameters['q_T1_T2'],
+             self.parameters['q_T2_A']]
 
         # calculate timestep-dependent updates
-        length_update = self.parameters["r"] * timestep
         phase_update = phase
+
+        if self.next_phase_change is None:
+            self.next_phase_change = exponential(1 / q[phase - 1])
+
+        self.next_phase_change -= timestep
+
+        if phase == Phase.ANAGEN:
+            length_update = self.parameters["r"] * timestep
+            if self.next_phase_change <= 0:
+                phase_update = Phase.CATAGEN
+                self.next_phase_change = None
+        elif phase == Phase.CATAGEN:
+            length_update = self.parameters["r_c"] * timestep
+            phase_update = phase
+            if self.next_phase_change <= 0:
+                phase_update = Phase.TELOGEN1
+                self.next_phase_change = None
+        elif phase == Phase.TELOGEN1:
+            length_update = 0
+            phase_update = phase
+            if self.next_phase_change <= 0:
+                phase_update = Phase.TELOGEN2
+                self.next_phase_change = None
+        else:  # phase == Phase.TELOGEN2:
+            length_update = -length
+            phase_update = phase
+            if self.next_phase_change <= 0:
+                phase_update = Phase.ANAGEN
+                self.next_phase_change = None
 
         # return an update that mirrors the ports structure
         return {
             'global': {
-                'length' : length_update,
-                'phase' : phase_update
+                'length': length_update,
+                'phase': phase_update
             }
         }
 
@@ -103,13 +144,13 @@ def run_template_process():
     initial_state = {
         'global': {
             'length': 0.0,
-            'phase': 'anagen'
+            'phase': Phase.ANAGEN
         },
-     }
+    }
 
     # run the simulation
     sim_settings = {
-        'total_time': 10,
+        'total_time': 10 * 365,
         'initial_state': initial_state}
     output = simulate_process_in_experiment(template_process, sim_settings)
 
